c6ac6bae7c452de1f81d4b012f7652f231170c4a
jibranc <jibran.c@utexas.edu>
2024-04-10 20:49:38 -0500

    added running averages in arrays but still need to make deltas

--- run.py
+++ run.py
@@ -15,8 +15,11 @@ def main():
 
   args = parser.parse_args()
 
-  if len(args.input) == 1:
+  if len(args.input) == 1 and args.input[0].isdigit():
       INPUT = int(args.input[0])
+  else:
+      INPUT = args.input[0]
+
   
   cap = cv2.VideoCapture(INPUT)
   Config.FPS = cap.get(cv2.CAP_PROP_FPS) # time tell shows should be 1 second
@@ -28,8 +31,16 @@ def main():
 
   face_mesh = mp.solutions.face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
   hands = mp.solutions.hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+  running_avg_BPM = []
+  running_avg_emotion = []
+  running_avg_hands = []
+  running_avg_lip = []
+  running_avg_gaze = []
+  running_avg_blink = []
+  counter = 0
   
   try:
+
     metrics_calculator = MetricsCalculator()
     while cap.isOpened():
       success, image = cap.read()
@@ -41,7 +52,22 @@ def main():
         continue
       
       metrics = metrics_calculator.collect_metrics(image, face_landmarks, hands_landmarks)
-      print("Metrics:", metrics)
+      print("Metrics: BPM, emotion, hands, lip, gaze, blink", metrics)
+      
+
+      #in a separate array, store the running average for each frame. This essentially just takes the average of the previous values
+      #and adds the current one and averages it out 
+      if(counter==0):
+        running_avg_lip.append(metrics[3])
+        running_avg_gaze.append(metrics[4])
+        running_avg_blink.append(metrics[5])
+
+      else:
+        running_avg_lip.append((metrics[3]+running_avg_lip[counter-1])/(counter+1))
+        running_avg_gaze.append((metrics[4]+running_avg_gaze[counter-1])/(counter+1))
+        running_avg_blink.append((metrics[5]+running_avg_blink[counter-1])/(counter+1))
+
+      counter+=1
       
       if args.mesh: draw_landmarks(image, face_landmarks, hands_landmarks)
       cv2.imshow('face', image)
